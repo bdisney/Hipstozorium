812b1e6b153d8fce8f23d51f773768dfeb91b476
disne_y <bold.disney@gmail.com>
2016-12-11 19:46:54 +0300

    Create table Posts

--- app.rb
+++ app.rb
@@ -12,6 +12,17 @@ before do
   init_db
 end
 
+configure do
+  init_db
+  @db.execute 'CREATE TABLE IF NOT EXISTS Posts
+    (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      created_date DATE,
+      content TEXT
+    )'
+end
+
+
 get '/new' do
   erb :new
 end
