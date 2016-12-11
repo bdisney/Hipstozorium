e9f8a59f4569fe355d71206f34cecd01dd69c35f
disne_y <bold.disney@gmail.com>
2016-12-11 19:26:26 +0300

    Init database, make @db

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'hipstozorium.db'
+  @db.results_as_hash = true
+end
+
+before do
+
+end
 
 get '/new' do
   erb :new
